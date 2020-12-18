9bde8390c9e1dd02308fd2e54b5a9c10086f8377
Julien Mairal <julien.mairal@inria.fr>
2020-12-18 21:13:46 +0100

    add spams solver and fix cyanure

--- solvers/cyanure.py
+++ solvers/cyanure.py
@@ -4,6 +4,7 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     from cyanure import Regression
+    import scipy
 
 
 class Solver(BaseSolver):
@@ -14,18 +15,20 @@ class Solver(BaseSolver):
 
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
+        if scipy.sparse.issparse(self.X) and scipy.sparse.isspmatrix_csc(self.X):
+            self.X = scipy.sparse.csr_matrix(self.X)
 
         n_samples = self.X.shape[0]
 
         self.solver = Regression(loss='square', penalty='l1',
                                  fit_intercept=False)
         self.solver_parameter = dict(
-            lambd=self.lmbd / n_samples, solver='auto',
+            lambd=self.lmbd / n_samples, solver='auto', it0=1000000,
             tol=1e-12, verbose=False
         )
 
     def run(self, n_iter):
-        self.solver.fit(self.X, self.y, max_epochs=n_iter,
+        self.solver.fit(self.X, self.y, max_epochs=n_iter, 
                         **self.solver_parameter)
 
     def get_result(self):
