8a95e6717cfbfd26526de1bd8610adc01b2e46b1
Tanguy Lefort <tanguy_l@outlook.fr>
2021-03-27 23:56:51 +0100

    fortran in conda

--- solvers/glmnet.py
+++ solvers/glmnet.py
@@ -18,7 +18,7 @@ class Solver(BaseSolver):
     name = "glmnet"
 
     install_cmd = 'conda'
-    requirements = ['r-base', 'rpy2', 'r-glmnet ', 'gcc_linux-64']
+    requirements = ['r-base', 'rpy2', 'r-glmnet ', 'libgfortran']
     references = [
         'J. Friedman, T. J. Hastie and R. Tibshirani, "Regularization paths '
         'for generalized linear models via coordinate descent", '
