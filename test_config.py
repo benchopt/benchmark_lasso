347eb162fa297fe03c03ec1c98fa4522dd11d773
tomMoral <thomas.moreau.2010@gmail.com>
2021-06-11 16:16:21 +0200

    ENH use correct names

--- test_config.py
+++ test_config.py
@@ -3,19 +3,27 @@ import sys
 import pytest
 
 
-def xfail_test_solver_install(solver_class):
+def check_test_solver_install(solver_class):
 
     if solver_class.name.lower() == 'cyanure' and sys.platform == 'darwin':
         pytest.xfail('Cyanure is not easy to install on macos.')
 
     # Skip test_solver_install for julia in OSX as there is a version
     # conflict with conda packages for R
-    # See issue benchopt/benchopt#64
+    # See issue benchopt/benchopt#64.
     if 'julia' in solver_class.name.lower() and sys.platform == 'darwin':
         pytest.xfail('Julia causes segfault on OSX for now.')
 
     # Lightning install is broken on python3.9+.
-    # See issue #XX
+    # See issue scikit-learn-contrib/lightning#153.
     if (solver_class.name.lower() == 'lightning'
             and sys.version_info >= (3, 9)):
         pytest.xfail('Lightning install is broken on python3.9+.')
+
+    # Lightning install is broken on python3.9+.
+    # See CEA-COSMIC/ModOpt#144.
+    if (solver_class.name.lower() == 'modopt'):
+        pytest.skip(
+            'Modopt breaks other package installation by changing '
+            'numpy version. Skipping for now.'
+        )
