53060a45924b3e8953d89c2fc2c046f1e9bcd876
mathurinm <mathurin.massias@gmail.com>
2021-09-29 13:04:15 +0200

    Remove boston dataset following sklearn deprecation, replace by leukemia

--- datasets/leukemia.py
+++ datasets/leukemia.py
@@ -4,20 +4,22 @@ from benchopt import safe_import_context
 
 
 with safe_import_context() as import_ctx:
-    from sklearn.datasets import load_boston
+    from sklearn.datasets import fetch_openml
+    from sklearn.preprocessing import LabelBinarizer
 
 
 class Dataset(BaseDataset):
 
-    name = "Boston"
+    name = "Leukemia"
 
     install_cmd = 'conda'
     requirements = ['scikit-learn']
 
     def get_data(self):
 
-        X, y = load_boston(return_X_y=True)
-
+        X, y = fetch_openml("leukemia", return_X_y=True)
+        X = X.to_numpy()
+        y = LabelBinarizer().fit_transform(y)[:, 0]
         data = dict(X=X, y=y)
 
         return X.shape[1], data
