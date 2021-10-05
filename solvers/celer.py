2f06395a40bbab34c668851f0555ff0b4cb99c9b
mathurinm <mathurin.massias@gmail.com>
2021-10-05 09:37:56 +0200

    rm normalize parameter in celer

--- solvers/celer.py
+++ solvers/celer.py
@@ -31,7 +31,7 @@ class Solver(BaseSolver):
         self.lasso = Lasso(
             alpha=self.lmbd / n_samples, max_iter=1, max_epochs=100000,
             tol=1e-12, prune=True, fit_intercept=fit_intercept,
-            normalize=False, warm_start=False, positive=False, verbose=False,
+            warm_start=False, positive=False, verbose=False,
         )
 
     def run(self, n_iter):
