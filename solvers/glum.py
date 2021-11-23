94127c1946b6f3cf0e4ac3d7761c92ad0cef0cfd
mathurinm <mathurin.massias@gmail.com>
2021-11-23 15:40:18 +0100

    this fails

--- solvers/glum.py
+++ solvers/glum.py
@@ -1,7 +1,7 @@
 import warnings
 from benchopt import BaseSolver
 from benchopt import safe_import_context
-
+from benchopt.stopping_criterion import SufficientProgressCriterion
 
 with safe_import_context() as import_ctx:
     import numpy as np
@@ -14,16 +14,18 @@ class Solver(BaseSolver):
 
     install_cmd = 'conda'
     requirements = ['glum']
-    stop_strategy = 'tolerance'
+    stop_strategy = SufficientProgressCriterion(
+        patience=5, strategy='tolerance')
 
     def set_objective(self, X, y, lmbd, fit_intercept):
         self.X, self.y, self.lmbd = X, y, lmbd
         self.fit_intercept = fit_intercept
         n_samples = self.X.shape[0]
-        # all coefs are 0 when starting, so we can take anything in
-        # [-alpha, alpha] as subgradient of penalty. The minimal norm
-        # subgradient of the objective is thus ST(grad, alpha), and its
-        # L1 norm is:
+        # glum's stopping criterion is on the L1 norm of the minimal norm
+        # subgradient of the objective. All coefs are 0 when starting, so
+        # anything in [-alpha, alpha] is a subgradient of the penalty.
+        # The minimal norm subgradient of the objective is thus
+        # soft_thresholding(X.T @ y / len(y), alpha), and its L1 norm is:
         self.tol_scale = np.sum(
             np.maximum(np.abs(X.T @ y / len(y)) - lmbd / n_samples, 0))
         self.model = GeneralizedLinearRegressor(
@@ -35,12 +37,15 @@ class Solver(BaseSolver):
         )
         warnings.filterwarnings('ignore', category=ConvergenceWarning)
 
+    def skip(self, X, y, lmbd, fit_intercept):
+        # glum instantiates a Hessian of size (n_features, n_features)
+        if X.shape[1] > 20_000:
+            return True, "glum does not support n_features >= 20000"
+        return False, None
+
     def run(self, tol):
-        # support sparse while avoiding MemoryError if n_features too large,
-        # as glum intanciates a Hessian
-        if self.X.shape[1] < 20_000:
-            self.model.gradient_tol = self.tol_scale * tol
-            self.model.fit(self.X, self.y)
+        self.model.gradient_tol = self.tol_scale * tol
+        self.model.fit(self.X, self.y)
 
     def get_result(self):
         beta = self.model.coef_.flatten()
