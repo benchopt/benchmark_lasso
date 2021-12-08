5fa72e90210697b7ced0a0d2b7559868f35db3dd
mathurinm <mathurin.massias@gmail.com>
2021-12-08 13:32:55 +0100

    more comments on glmnet behavior

--- solvers/glmnet.py
+++ solvers/glmnet.py
@@ -29,6 +29,8 @@ class Solver(BaseSolver):
 
     stopping_criterion = SufficientProgressCriterion(
         patience=7, eps=1e-38, strategy='tolerance')
+    # We use the tolerance strategy because if maxit is too low and glmnet
+    # convergence check fails, it returns an empty model
 
     def skip(self, X, y, lmbd, fit_intercept):
         # XXX - glmnet support intercept, adapt the API
@@ -44,7 +46,7 @@ class Solver(BaseSolver):
         self.glmnet = robjects.r['glmnet']
 
     def run(self, tol):
-        # even if maxit=0, glmnet can return non zero coefficients. To get the
+        # Even if maxit=0, glmnet can return non zero coefficients. To get the
         # initial point on the curve, we set tol=0: this way, glmnet
         # convergence check fails, and rather than returning the current
         # iterate, it returns a 0 model.
@@ -57,6 +59,10 @@ class Solver(BaseSolver):
             # plateau before convergence
             thresh = tol ** 1.8
 
+        # We fit with a single lambda because when computing a path, glmnet
+        # early stops the path based on an uncontrollable criterion. Fitting
+        # with a single lambda may be suboptimal if it is small, but there is
+        # no other way to force glmnet to solve for a prescribed lambda.
         fit_dict = {"lambda": self.lmbd / len(self.y)}
 
         glmnet_fit = self.glmnet(self.X, self.y, intercept=False,
