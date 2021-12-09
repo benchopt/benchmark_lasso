5b1bec95269e2e50a1c81c0c68a2d630859cfa20
Johan Larsson <13087841+jolars@users.noreply.github.com>
2021-12-09 12:03:44 +0100

    FIX proper convergence checks for glmnet solver (#46)
    Co-authored-by: mathurinm <mathurin.massias@gmail.com>
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- solvers/glmnet.py
+++ solvers/glmnet.py
@@ -1,5 +1,6 @@
-from benchopt import BaseSolver
-from benchopt import safe_import_context
+from benchopt import BaseSolver, safe_import_context
+from benchopt.runner import INFINITY
+from benchopt.stopping_criterion import SufficientProgressCriterion
 
 
 with safe_import_context() as import_ctx:
@@ -24,8 +25,13 @@ class Solver(BaseSolver):
         'for generalized linear models via coordinate descent", '
         'J. Stat. Softw., vol. 33, no. 1, pp. 1-22, NIH Public Access (2010)'
     ]
-    stop_strategy = 'iteration'
     support_sparse = False
+
+    # We use the tolerance strategy because if maxit is too low and glmnet
+    # convergence check fails, it returns an empty model
+    stopping_criterion = SufficientProgressCriterion(
+        patience=7, eps=1e-38, strategy='tolerance'
+    )
 
     def skip(self, X, y, lmbd, fit_intercept):
         # XXX - glmnet support intercept, adapt the API
@@ -38,18 +44,35 @@ class Solver(BaseSolver):
         self.X, self.y, self.lmbd = X, y, lmbd
         self.fit_intercept = fit_intercept
 
-        self.lmbd_max = np.max(np.abs(X.T @ y))
         self.glmnet = robjects.r['glmnet']
 
-    def run(self, n_iter):
-        fit_dict = {"lambda.min.ratio": self.lmbd / self.lmbd_max}
+    def run(self, tol):
+        # Even if maxit=0, glmnet can return non zero coefficients. To get the
+        # initial point on the curve, we set tol=0: this way, glmnet
+        # convergence check fails, and rather than returning the current
+        # iterate, it returns a 0 model.
+        if tol == INFINITY:
+            maxit = 0
+            thresh = 0
+        else:
+            maxit = 1_000_000
+            # we need thresh to decay fast, otherwise the objective curve can
+            # plateau before convergence
+            thresh = tol ** 1.8
+
+        # We fit with a single lambda because when computing a path, glmnet
+        # early stops the path based on an uncontrollable criterion. Fitting
+        # with a single lambda may be suboptimal if it is small, but there is
+        # no other way to force glmnet to solve for a prescribed lambda.
+        fit_dict = {"lambda": self.lmbd / len(self.y)}
+
         glmnet_fit = self.glmnet(self.X, self.y, intercept=False,
-                                 standardize=False, maxit=n_iter,
-                                 thresh=1e-14, **fit_dict)
+                                 standardize=False, maxit=maxit,
+                                 thresh=thresh, **fit_dict)
         results = dict(zip(glmnet_fit.names, list(glmnet_fit)))
         as_matrix = robjects.r['as']
         coefs = np.array(as_matrix(results["beta"], "matrix"))
-        self.w = coefs[:, -1]
+        self.w = coefs.flatten()
 
     def get_result(self):
         return self.w
