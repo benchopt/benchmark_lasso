fc436d1a1ba189389d88cd42280c8d6899faa3a9
mathurinm <mathurin.massias@gmail.com>
2022-01-12 17:05:55 +0100

    smaller simulated

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -12,10 +12,10 @@ class Dataset(BaseDataset):
     # the cross product for each key in the dictionary.
     parameters = {
         'n_samples, n_features': [
-            (100, 5_000),
-            (100, 10_000)
+            (200, 500),
+            # (100, 10_000)
         ],
-        'rho': [0, 0.6],
+        'rho': [0.6],
     }
 
     def __init__(self, n_samples=10, n_features=50, rho=0, random_state=27):
