c48d9f044845faf3b6e614390e2c412e27e11863
mathurinm <mathurin.massias@gmail.com>
2022-01-12 16:04:04 +0100

    try an alternate approach to efficient solve, without forming X.T @ X

--- solvers/noncvx_pro.py
+++ solvers/noncvx_pro.py
@@ -4,25 +4,44 @@ from benchopt import safe_import_context
 with safe_import_context() as import_ctx:
     import numpy as np
     import scipy.optimize as sciop
+    from scipy.sparse import linalg as slinalg
 
 
 class Solver(BaseSolver):
-    name = "Noncvx-Pro"
+    name = "noncvx-pro"
 
     stop_strategy = 'iteration'
     support_sparse = False
+    parameters = {'old': [True, False]}
 
-    def set_objective(self, X, y, lmbd):
+    def set_objective(self, X, y, lmbd, fit_intercept):
         self.X, self.y, self.lmbd = X, y, lmbd
 
-    def efficient_solve(self, X, y, lmbd):
-        n_samples, n_features = X.shape
-        if n_samples >= n_features:
-            M = lmbd * np.eye(n_features) + np.dot(X.T, X)
-            v1 = np.linalg.solve(M, X.T @ y)
+    def efficient_solve(self, A, b, lmbd):
+        """Solve (A.T @ A + lmbd Id) x = b without forming the LHS matrix."""
+        n_samples, n_features = A.shape
+        if self.old:
+            if n_samples >= n_features:
+                M = lmbd * np.eye(n_features) + np.dot(A.T, A)
+                v1 = np.linalg.solve(M, A.T @ b)
+            else:
+                M = lmbd * np.eye(n_samples) + np.dot(A, A.T)
+                v1 = A.T @ np.linalg.solve(M, b)
         else:
-            M = lmbd * np.eye(n_samples) + np.dot(X, X.T)
-            v1 = X.T @ np.linalg.solve(M, y)
+            if n_samples >= n_features:
+                def mv(x):
+                    return lmbd * x + A.T @ (A @ x)
+                linop = slinalg.LinearOperator(
+                    shape=(n_features, n_features), matvec=mv
+                )
+                v1 = slinalg.cg(linop, A.T @ b)[0]
+            else:
+                def mv(z):
+                    return lmbd * z + A @ (A.T @ z)
+                linop = slinalg.LinearOperator(
+                    shape=(n_samples, n_samples), matvec=mv
+                )
+                v1 = A.T @ slinalg.cg(linop, b)[0]
         return v1
 
     def run(self, n_iter):
