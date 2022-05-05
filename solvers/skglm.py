75cd5538d75ea69ecca5c95e93fbde4099ed519b
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2022-05-05 16:29:00 +0200

    Added skip function for intercept skglm

--- solvers/skglm.py
+++ solvers/skglm.py
@@ -24,6 +24,13 @@ class Solver(BaseSolver):
         'https://arxiv.org/abs/2204.07826'
     ]
 
+    def skip(self, X, y, lmbd, fit_intercept):
+        # XXX - not implemented but this should be quite easy
+        if fit_intercept:
+            return True, f"{self.name} does not handle fit_intercept"
+
+        return False, None
+
     def set_objective(self, X, y, lmbd, fit_intercept):
         self.X, self.y, self.lmbd = X, y, lmbd
         self.fit_intercept = fit_intercept
