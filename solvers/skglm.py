3157b66c98647944d8276c3ce5969e1d3a2fea3a
mathurinm <mathurinm@users.noreply.github.com>
2022-05-05 16:31:32 +0200

    Update solvers/skglm.py

--- solvers/skglm.py
+++ solvers/skglm.py
@@ -25,7 +25,6 @@ class Solver(BaseSolver):
     ]
 
     def skip(self, X, y, lmbd, fit_intercept):
-        # XXX - not implemented but this should be quite easy
         if fit_intercept:
             return True, f"{self.name} does not handle fit_intercept"
 
