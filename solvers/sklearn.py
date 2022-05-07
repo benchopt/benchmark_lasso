51d04a9292732f9e77cf28a4cdeda7050c5f1643
mathurinm <mathurin.massias@gmail.com>
2022-05-07 10:44:37 +0200

    same changes for sklearn and skglm

--- solvers/sklearn.py
+++ solvers/sklearn.py
@@ -33,11 +33,16 @@ class Solver(BaseSolver):
         warnings.filterwarnings('ignore', category=ConvergenceWarning)
 
     def run(self, n_iter):
-        self.clf.max_iter = n_iter
-        self.clf.fit(self.X, self.y)
+        if n_iter == 0:
+            self.coef = np.zeros([self.X.shape[1] + self.fit_intercept])
+        else:
+            self.lasso.max_iter = n_iter
+            self.lasso.fit(self.X, self.y)
+
+            coef = self.lasso.coef_.flatten()
+            if self.fit_intercept:
+                coef = np.r_[coef, self.lasso.intercept_]
+            self.coef = coef
 
     def get_result(self):
-        beta = self.clf.coef_.flatten()
-        if self.fit_intercept:
-            beta = np.r_[beta, self.clf.intercept_]
-        return beta
+        return self.coef
