99f08ff268c53368ac68e2d50db990b50175f7c1
Joseph Salmon <joseph.salmon@umontpellier.fr>
2022-05-10 16:30:41 +0200

    adding preliminary version of pathological path example

---
+++ datasets/pathological.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from benchopt import BaseDataset
+from benchopt.datasets.simulated import make_correlated_data
+
+
+class Dataset(BaseDataset):
+
+    name = "Pathological"
+
+    # List of parameters to generate the datasets. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    parameters = {
+        'n_samples': [
+            11,
+            4
+        ],
+    }
+
+    def __init__(self, n_samples=11, random_state=27):
+        # Store the parameters of the dataset
+        self.n_samples = n_samples
+        self.n_features = n_samples
+        self.rng = np.random.RandomState(random_state)
+        self.diago = self.rng.rand(n_samples)
+
+    def get_data(self):
+        y = np.ones(self.n_samples)
+        tri = 2 * np.triu(np.ones([self.n_samples, self.n_samples]))
+        print(self.diago.shape)
+        X = tri * self.diago - np.diag(self.diago)
+        data = dict(X=X, y=y)
+
+        return self.n_features, data
