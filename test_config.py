2f690b7cad51c08c0a02d3a2d4ed0a8fa3044e3a
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-12 13:55:33 +0200

    typo

--- test_config.py
+++ test_config.py
@@ -1,7 +1,7 @@
 import sys
 import pytest
 
- from benchopt.utils.sys_info import _get_cuda_version
+from benchopt.utils.sys_info import _get_cuda_version
 
 
 def check_test_solver_install(solver_class):
