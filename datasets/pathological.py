531be203de76b1158df37902122b2d867e099829
Joseph Salmon <joseph.salmon@umontpellier.fr>
2022-05-12 23:03:27 +0200

    smaller valuers for diago

--- datasets/pathological.py
+++ datasets/pathological.py
@@ -12,7 +12,7 @@ class Dataset(BaseDataset):
     parameters = {
         'n_samples': [
             11,
-            4
+            1000
         ],
     }
 
@@ -20,13 +20,13 @@ class Dataset(BaseDataset):
         # Store the parameters of the dataset
         self.n_samples = n_samples
         self.n_features = n_samples
-        self.rng = np.random.RandomState(random_state)
-        self.diago = self.rng.rand(n_samples)
+        # self.rng = np.random.RandomState(random_state)
+        # self.diago = self.rng.rand(n_samples)
+        self.diago = np.logspace(-6, 18, n_samples)
 
     def get_data(self):
         y = np.ones(self.n_samples)
         tri = 2 * np.triu(np.ones([self.n_samples, self.n_samples]))
-        print(self.diago.shape)
         X = tri * self.diago - np.diag(self.diago)
         data = dict(X=X, y=y)
 
