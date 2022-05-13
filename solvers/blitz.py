4a6fedabae3d37924df05dc639155d344fdb4861
Cassio F. Dantas <cassiofragadantas@gmail.com>
2022-05-13 14:08:31 +0200

    fit_intercept for blitz solver (supports sparse data!)

--- solvers/blitz.py
+++ solvers/blitz.py
@@ -4,6 +4,7 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import blitzl1
+    import numpy as np
 
 
 class Solver(BaseSolver):
@@ -20,16 +21,11 @@ class Solver(BaseSolver):
         'vol. 37, pp. 1171-1179 (2015)'
     ]
 
-    def skip(self, X, y, lmbd, fit_intercept):
-        if fit_intercept:
-            return True, f"{self.name} does not handle fit_intercept"
-
-        return False, None
-
     def set_objective(self, X, y, lmbd, fit_intercept):
         self.X, self.y, self.lmbd = X, y, lmbd
+        self.fit_intercept = fit_intercept
 
-        blitzl1.set_use_intercept(False)
+        blitzl1.set_use_intercept(self.fit_intercept)
         blitzl1.set_tolerance(0)
         self.problem = blitzl1.LassoProblem(self.X, self.y)
 
@@ -39,7 +35,10 @@ class Solver(BaseSolver):
         return previous + 1
 
     def run(self, n_iter):
-        self.coef_ = self.problem.solve(self.lmbd, max_iter=n_iter).x
+        self.sol_ = self.problem.solve(self.lmbd, max_iter=n_iter)
 
     def get_result(self):
-        return self.coef_.flatten()
+        if self.fit_intercept:
+            return np.r_[self.sol_.x, self.sol_.intercept]
+        else:
+            return self.sol_.x.flatten()
