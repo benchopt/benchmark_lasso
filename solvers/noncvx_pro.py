7576b8f88bac45a04de5de5a493df950b73a7e93
mathurinm <mathurin.massias@gmail.com>
2022-05-17 11:41:14 +0200

    fix noncvxpro when n_samples=n_features

--- solvers/noncvx_pro.py
+++ solvers/noncvx_pro.py
@@ -20,7 +20,7 @@ class Solver(BaseSolver):
 
     def set_objective(self, X, y, lmbd, fit_intercept):
         self.X, self.y, self.lmbd = X, y, lmbd
-        if X.shape[0] > X.shape[1]:
+        if X.shape[0] >= X.shape[1]:
             self.C = X.T @ X
             if issparse(self.C):
                 self.C = self.C.toarray()
