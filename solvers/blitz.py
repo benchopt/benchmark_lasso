4e7f2e2e811e253fb452347cd90668c48a393c89
mathurinm <mathurin.massias@gmail.com>
2022-05-18 07:45:40 +0200

    blitz stop crit

--- solvers/blitz.py
+++ solvers/blitz.py
@@ -1,5 +1,6 @@
 from benchopt import BaseSolver
 from benchopt import safe_import_context
+from benchopt.stopping_criterion import SufficientDescentCriterion
 
 
 with safe_import_context() as import_ctx:
@@ -10,6 +11,7 @@ with safe_import_context() as import_ctx:
 class Solver(BaseSolver):
     name = 'Blitz'
     stopping_strategy = 'iteration'
+    stopping_criterion = SufficientDescentCriterion(eps=1e-12, patience=4)
 
     install_cmd = 'conda'
     requirements = [
