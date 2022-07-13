442aa63865213e0fabebdbdbd0ba134d5d07d057
mathurinm <mathurin.massias@gmail.com>
2022-07-13 09:52:21 +0200

    remove modopt test skip

--- test_config.py
+++ test_config.py
@@ -15,14 +15,6 @@ def check_test_solver_install(solver_class):
     if 'julia' in solver_class.name.lower():
         pytest.xfail('Julia install from conda fails currently.')
 
-    # ModOpt install change numpy version, breaking celer install.
-    # See CEA-COSMIC/ModOpt#144. Skipping for now
-    if ('modopt' in solver_class.name.lower()):
-        pytest.skip(
-            'Modopt breaks other package installation by changing '
-            'numpy version. Skipping for now.'
-        )
-
     if "cuml" in solver_class.name.lower():
         if sys.platform == "darwin":
             pytest.xfail("Cuml is not supported on MacOS.")
