1ecf03583447ea795364bc3e40b257437dea971a
Theo Guyard <theo.guyard@insa-rennes.fr>
2022-11-09 11:45:56 +0100

    Type julia_pgd

--- solvers/julia_pgd.jl
+++ solvers/julia_pgd.jl
@@ -1,22 +1,25 @@
 using Core
 using LinearAlgebra
 
+st(w::Vector{Float64}, t::Float64) = @. sign(w) * max(abs(w) - t, 0.)
 
-function st(w, t)
-    w = map(sign, w) .* map(x -> max(abs(x) - t, 0), w)
-end
-
-
-function solve_lasso(X, y, lambda, n_iter)
+function solve_lasso(
+    X::Matrix{Float64}, 
+    y::Vector{Float64}, 
+    lambda::Float64,
+    n_iter::Int
+)
     L = opnorm(X)^2
-
-    n_features = size(X, 2)
-    w = zeros(n_features, 1)
-    t_new = 1
+    p = size(X, 2)
+    w = zeros(p)
+    r = similar(y)
+    v = similar(w)
+    g = similar(w)                  
     for i ∈ 1:n_iter
-        grad = X' * (X * w - y)
-        w -= grad / L
-        w = st(w, lambda / L)
+        r = X * w - y
+        g = X' * r
+        v = w - g / L
+        w = st(v, lambda / L)
     end
 
     return w
