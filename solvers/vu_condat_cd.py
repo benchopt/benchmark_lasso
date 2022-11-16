7d76d093a134f58eb44d1517f8e452b3f2e292a5
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-16 13:58:04 +0100

    unwrap lasso && add refs

--- solvers/vu_condat_cd.py
+++ solvers/vu_condat_cd.py
@@ -3,7 +3,7 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
-    from cd_solver.sklearn_api import Lasso
+    from cd_solver import Problem, coordinate_descent
 
 
 class Solver(BaseSolver):
@@ -11,15 +11,21 @@ class Solver(BaseSolver):
 
     install_cmd = 'conda'
     requirements = [
-        'pip:git+https://github.com/Badr-MOUFAD/Fercoq-Bianchi-solver.git@sklearn-api'
+        'pip:'
+        'git+https://github.com/Badr-MOUFAD/Fercoq-Bianchi-solver.git@main'
     ]
 
     references = [
         "Olivier Fercoq and Pascal Bianchi, "
-        "'A coordinate descent primal-dual algorithm with large step size and possibly non separable functions', "
+        "'A coordinate descent primal-dual algorithm with large step size "
+        "and possibly non separable functions', "
         "SIAM Journal on Optimization, 29(1), 100-134, "
         "https://arxiv.org/pdf/1508.04625.pdf, "
         "code: https://github.com/Badr-MOUFAD/Fercoq-Bianchi-solver",
+        "Olivier Fercoq, "
+        "'A generic coordinate descent solver for nonsmooth convex optimization', "
+        "Optimization Methods and Software, 2019, pp.1-21, "
+        "https://hal.archives-ouvertes.fr/hal-01941152v2/document"
     ]
 
     parameters = {
@@ -33,24 +39,41 @@ class Solver(BaseSolver):
         self.X, self.y = X, y
         self.lmbd = lmbd
 
-        # when smooth_formulation=True, the estimator considers the datafit term smooth
-        # and uses it gradient to find the min. Otherwise, estimator considers
-        # the datafit non-smooth and uses it prox operator instead
-        self.estimator = Lasso(
-            alpha=lmbd,
-            smooth_formulation=self.smooth_formulation
-        )
+        # when smooth_formulation=True, the estimator considers
+        # the datafit term smooth and uses its gradient to find the min.
+        # Otherwise, estimator considers the datafit non-smooth
+        # and uses it prox operator instead
+        n_samples = X.shape[0]
+        if self.smooth_formulation:
+            self.datafit_params = {
+                'f': ["square"], 'Af': X, 'bf': y,
+                'blocks_f': [0, n_samples],
+                'cf': [0.5],
+            }
+        else:
+            self.datafit_params = {
+                'h': ["square"], 'Ah': X, 'bh': y,
+                'blocks_h': [0, n_samples],
+                'ch': [0.5],
+            }
 
     def run(self, n_iter):
-        X, y = self.X, self.y
+        n_features = self.X.shape[1]
 
         if n_iter == 0:
-            self.w = np.zeros(X.shape[1])
+            self.w = np.zeros(n_features)
             return
 
-        self.estimator.max_iter = n_iter
-        self.estimator.fit(X, y)
-        self.w = self.estimator.coef_.flatten()
+        pb = Problem(
+            N=n_features,
+            # datafit
+            **self.datafit_params,
+            # penalty
+            g=["abs"] * n_features, cg=[self.lmbd] * n_features
+        )
+        coordinate_descent(pb, max_iter=n_iter, per_pass=1)
+
+        self.w = pb.sol.flatten()
 
     def get_result(self):
         return self.w
